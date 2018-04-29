d3eca931846be533446dc749fa64d70de8345afb
Karl Pokus <karl.pokus@gmail.com>
2018-04-29 20:06:18 +0200

    add syntax hilight for txt

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 # stolen from http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 # This script creates symlinks from the home directory to any desired dotfiles in $dir
 # places old files in $olddir as backup
 
 # Todos
-# [ ] fix repeated use. Don't move previously added symlinks to ~ too
-# [ ] get files from /src with $(ls)
+# fix repeated use. Don't move previously added symlinks to ~ too
+# make idempotent
 
 # Requirements
 # - this repo in ~/dotfiles
@@ -15,14 +16,20 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 files=".bash_profile .vimrc .gitconfig .tmux.conf"
 
-# create dotfiles_old in homedir - idempotent
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -pv $olddir && echo "done"
+# create backup dir
+echo "creating $olddir"
+mkdir -pv $olddir && echo done
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+echo "moving dotfiles to $olddir and symlinking"
 for file in $files; do
-  echo "Moving any existing dotfile in ~ to $olddir"
-  mv -v ~/$file $olddir
-  echo "Creating symlink to $file in ~"
+  mv -iv ~/$file $olddir
   ln -sv $dir/src/$file ~/$file
 done
+echo done
+
+# move ~/.vim to backup
+echo "moving ~/.vim to $olddir and symlinking"
+mv -iv ~/.vim $olddir 
+ln -sv $dir/vim/.vim ~/.vim 
+echo done
--- src/.vimrc
+++ src/.vimrc
@@ -18,23 +18,33 @@ set shiftround            " always indent/outdent to the nearest tabstop
 set expandtab             " use spaces instead of tabs
 set smarttab              " use tabs at the start of a line, spaces elsewhere
 set nowrap                " don't wrap text
-
-" tests
 set path=$PWD/**          " set path to current dir from where you launched vim
+
+" highlight for .txt
+highlight TxtComment    ctermfg=19
+highlight TxtDone       ctermfg=19
+highlight TxtPending    ctermfg=green
+highlight TxtHeader     ctermfg=green
+
 " file browser
 let g:netrw_banner = 0	  " remove banner
 let g:netrw_liststyle=3   " tree view
-set wildmenu              " enabled menu at bottom. hit TAB twice
+set wildmenu              " enabled menu at bottom 
 
+" KEY MAPPINGS
+let mapleader=","
 " tabs
 nnoremap <leader><Tab> :tabn<CR>
 nnoremap <leader><S-Tab> :tabp<CR>
 nnoremap <leader>x :tabclose<CR>
-
-" KEY MAPS
-let mapleader=","
 " open file browser
 nnoremap <leader>d :Sex<CR>
 " copy to system clipboard
 vnoremap <silent> <leader>c :!tee >(pbcopy)<CR>
+" edit vimrc
+nnoremap <leader>ev :vs $MYVIMRC<cr>
+" source vimrc
+nnoremap <leader>sv :so $MYVIMRC<cr>
+" use space to page down
+nnoremap <space> <c-d>
 
