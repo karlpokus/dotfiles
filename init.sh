b91c58c1c8e40cc866da5814f2036cfe02762c7c
Karl Pokus <karl.pokus@gmail.com>
2019-05-19 12:26:24 +0200

    fix conditional bug

--- init.sh
+++ init.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # stolen from http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
-# This script creates symlinks from the home directory to any desired dotfiles in $dir
-# places old files in $olddir as backup
-
-# Todos
-# make idempotent
+# This script creates symlinks from a dotfile to the users home dir
+# If the dotfile already exists - but is no symlink - we move it to a backup dir
 
 # Requirements
 # this repo in ~/dotfiles
@@ -15,15 +12,21 @@ dir=~/dotfiles
 backup=/tmp/dotfiles_old
 targets=".bash_profile .vimrc .gitconfig .tmux.conf .vim"
 
-echo "running init.."
-
 # create backup dir if neccessary
 mkdir -pv $backup
 
 # move any non-links to backup and create symlinks
 for target in $targets; do
-  if [ ! -h ~/$target ]; then
-    mv -iv ~/$target $backup
+  if test -e ~/$target; then # file exists
+    if test ! -h ~/$target; then # file is no symlink
+      echo "moving $target to $backup and creating a symlink"
+      mv -iv ~/$target $backup
+      ln -sv $dir/src/$target ~/$target
+    else
+      echo "no op $target is symlink"
+    fi
+  else
+    echo "creating symlink for $target"
     ln -sv $dir/src/$target ~/$target
   fi
 done
